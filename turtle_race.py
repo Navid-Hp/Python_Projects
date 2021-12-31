ee0750c75f17dd37fe4c25c16ab363c9dbee8b4b
Navid <navid.hosseinipour@gmail.com>
2021-12-31 13:42:08 +0200

    turtle_race
    This program is a simple graphical Turtle Race where you can bet on one of the six colored turtles by typing their color names. There are six color names in total containing all the colors in the rainbow :)

---
+++ turtle_race.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+# this for loop is for positioning all the turtle at the start position of the race and assigning appropriate colors from the colors list
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+# this condition is to ensure that the game won't start without actually receiving a bet from the user
+if user_bet:
+    is_race_on = True
+# The goal of this while loop is for ending the race as soon as a turtle reaches the end line. Then print messages to the user accordingly. 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! the {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! the {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
