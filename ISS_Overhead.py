ccc0f954cc2e21bcf1e5b0183026f3af24b46bc9
Navid <navid.hosseinipour@gmail.com>
2022-01-14 14:04:55 +0200

    ISS_Overhead
    This program will help you see the ISS in the sky by sending a notification to your email telling you to look up for it if the ISS is approximately above you and it is night time when you can actually see it in the sky.

---
+++ ISS_Overhead.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+# For this program to work properly, you should add in your actual email and password as well as your latitude and longitude to the constants below.
+# You can easily get your latitude and longitude from www.latlong.net by just entering the name of your city and clicking on the FIND button.
+MY_EMAIL = ""
+PASSWORD = ""
+MY_LAT = 56.949650
+MY_LONG = 24.105186
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if (MY_LAT - 5) <= iss_latitude <= (MY_LAT + 5) and (MY_LONG - 5) <= iss_longitude <= (MY_LONG + 5):
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+# sending an email notificaiton to ourselves if the ISS is approximately above us AND it is night time when we could see it in the sky.
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com", port=587)
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg="Subject:LOOK IP!\n\nThe ISS is above you in the sky. Look at the sky now!")
